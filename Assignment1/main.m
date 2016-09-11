4cd05ca110f47943feed5c879d9be9ea3adbc6b0
David Dang <ddang.csu@csu.fullerton.edu>
2016-09-11 12:07:50 -0700

    Initialized Assigment 1

---
+++ Assignment1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment1
+//
+//  Created by david on 9/11/16.
+//  Copyright © 2016 David Dang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
